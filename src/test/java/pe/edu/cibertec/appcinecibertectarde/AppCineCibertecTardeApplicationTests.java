5e5a748b5c54c2866a89ce0915e07febdc46786f
santizafer <santizafer@gmail.com>
2023-10-22 14:16:24 -0500

    initial commit

---
+++ src/test/java/pe/edu/cibertec/appcinecibertectarde/AppCineCibertecTardeApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appcinecibertectarde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCineCibertecTardeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
